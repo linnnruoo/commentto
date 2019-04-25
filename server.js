da99578562b6a50975ef653bc13ffc9b585902d3
linnnruoo <linruo2014@gmail.com>
2019-04-26 01:24:12 +0800

    modify config for heroku build

--- server.js
+++ server.js
@@ -28,4 +28,8 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+// listen to the port of the live server
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
 module.exports = app;
