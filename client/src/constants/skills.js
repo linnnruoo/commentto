6fcc1460b14c1db53485e73ee5a8a59de899e322
linnnruoo <linruo2014@gmail.com>
2019-04-25 22:13:24 +0800

    add asset and constant

---
+++ client/src/constants/skills.js
@@ -0,0 +1,14 @@
+const skills = {
+  Languages: ["C/C++", "JavaScript", "Python", "Java", "Verilog"],
+  Web: [
+    "React",
+    "Redux",
+    "Express",
+    "PostgreSQL",
+    "Apollo Client",
+    "React Native"
+  ],
+  UI: ["Material UI", "Bootstrap"]
+};
+
+export default skills;
