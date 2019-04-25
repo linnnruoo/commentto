64e1e673b932f38497c2743f10c3fb9c6791cd33
linnnruoo <linruo2014@gmail.com>
2019-04-25 16:32:57 +0800

    add api routes for post and reply

--- server/routes/api/posts/index.js
+++ server/routes/api/posts/index.js
@@ -1,4 +1,40 @@
 const express = require("express");
 const router = express.Router();
 
+const PostModel = require("../../../model/Post");
+
+/**
+ * @route GET /api/posts/
+ * @desc: Get all the posts
+ * @access Public
+ */
+router.get("/", async (req, res) => {
+  PostModel.getAll()
+    .then(posts => res.json(posts))
+    .catch(err =>
+      res.status(400).json({
+        status: false,
+        message: "Failed to retrieve posts"
+      })
+    );
+});
+
+/**
+ * @route POST /api/posts/
+ * @desc: create a new post
+ * @access Public
+ */
+router.post("/", async (req, res) => {
+  const { name, region, comment } = req.body;
+
+  PostModel.createOne(name, region, comment)
+    .then(post => res.json(post))
+    .catch(err =>
+      res.status(500).json({
+        status: false,
+        message: "Failed to create a new post"
+      })
+    );
+});
+
 module.exports = router;
--- server/routes/api/replies/index.js
+++ server/routes/api/replies/index.js
@@ -1,4 +1,40 @@
 const express = require("express");
 const router = express.Router();
 
+const ReplyModel = require("../../../model/Reply");
+
+/**
+ * @route GET /api/replies/
+ * @desc: Get all the replies
+ * @access Public
+ */
+router.get("/", async (req, res) => {
+  ReplyModel.getAll()
+    .then(posts => res.json(posts))
+    .catch(err =>
+      res.status(400).json({
+        status: false,
+        message: "Failed to retrieve posts"
+      })
+    );
+});
+
+/**
+ * @route POST /api/replies/
+ * @desc: add a new reply to a post
+ * @access Public
+ */
+router.post("/", async (req, res) => {
+  const { pid, name, comment } = req.body;
+
+  ReplyModel.createOne(pid, name, comment)
+    .then(reply => res.json(reply))
+    .catch(err =>
+      res.status(500).json({
+        status: false,
+        message: "Failed to add a reply"
+      })
+    );
+});
+
 module.exports = router;
