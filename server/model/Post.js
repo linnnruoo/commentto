570d5fd62b3aac0d350113ed472b0acde1a98a73
linnnruoo <linruo2014@gmail.com>
2019-04-25 16:23:24 +0800

    add Post and Reply models

---
+++ server/model/Post.js
@@ -0,0 +1,28 @@
+const db = require("../config/db");
+
+const Post = {
+  getAll: async () => {
+    const getAllPostsQuery = `SELECT * FROM posts`;
+    const { rows } = await db.query(getAllPostsQuery);
+    return rows;
+  },
+
+  createOne: async (name, region, comment) => {
+    const insertQuery = `INSERT INTO posts
+        VALUES($1, $2, $3)
+        returning *`;
+
+    const values = [name, region, comment];
+
+    try {
+      const { rows } = await db.query(insertQuery, values);
+
+      return rows[0];
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+};
+
+module.exports = Post;
---
+++ server/model/Reply.js
@@ -0,0 +1,32 @@
+const db = require("../config/db");
+
+const Reply = {
+  getAll: async () => {
+    const getQuery = `
+        SELECT * FROM replies
+    `;
+    const { rows } = await db.query(getQuery);
+    return rows;
+  },
+
+  createOne: async (pid, name, comment) => {
+    const insertQuery = `
+        INSERT INTO replies
+        VALUES($1, $2, $3)
+        returning *
+    `;
+
+    const values = [pid, name, comment];
+
+    try {
+      const { rows } = await db.query(insertQuery, values);
+
+      return rows[0];
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+};
+
+module.exports = Reply;
